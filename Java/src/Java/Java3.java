e8a614fc4299b115cc39b48a8b4a26cdad68b1e4
Nattakorn1 <natta.delltwin@gmail.com>
2021-03-10 14:48:49 +0700

    Java41

---
+++ Java/src/Java/Java3.java
@@ -0,0 +1,39 @@
+package Java;
+
+public class Java3 {
+
+	public static void main(String[] args) {
+		//3.1
+			int i = 20;
+			//3.1.1 a
+			i++;
+			i++;
+			i++;
+			i++;
+			System.out.println(i);
+			//3.1.1 b
+			--i;
+			--i;
+			--i;
+			--i;
+			--i;
+			System.out.println(i);
+		//3.2
+			float a = 1.1f;
+			float b = 1.2f;
+			if ((a == 1) && (b == 2)) {
+				System.out.println("True");
+			}else {
+			System.out.println("False");
+			}
+		//3.3
+			char a1 = '\u0001';
+			char b1 = '\u0023';
+			if ((a1 == '\u5535') || (b1 == '\u0023')) {
+				System.out.println("True");
+			}
+			
+
+	}
+
+}
---
+++ Java/src/Java/java41.java
@@ -0,0 +1,27 @@
+package Java;
+
+public class java41 {
+
+	public static void main(String[] args) {
+		//4.1.1
+		int score = 70;
+			//4.1 a 
+			if (score >= 80) {
+				System.out.println("Good");
+			}
+			//4.1 b
+			else if (score >= 50 && score <= 80){
+				System.out.println("Normal");
+			}
+			//4.1 c
+			else if (score < 50) {
+				System.out.println("Fail");
+			}
+		//4.2
+		boolean isHandSome = true;
+			if (isHandSome) {
+				System.out.println("Good");
+			}
+
+	}
+}
