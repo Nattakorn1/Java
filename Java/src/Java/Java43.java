b49c0c7cf9ef4ebf9112ab18ae0183417757014c
Nattakorn1 <natta.delltwin@gmail.com>
2021-03-10 16:51:22 +0700

    Java43

---
+++ Java/src/Java/Java43.java
@@ -0,0 +1,28 @@
+package Java;
+
+public class Java43 {
+
+	public static void main(String[] args) {
+		// 4.3
+		int grade = 30;
+			if (grade > 80) {
+				System.out.println("A");
+			}
+			else if (grade > 70 ){
+				System.out.println("B");
+			}
+			else if (grade > 60) {
+				System.out.println("C");
+			}else if (grade > 70){
+				System.out.println("B");
+			}
+			else if (grade > 50) {
+				System.out.println("C");
+			}else if (grade > 40 ){
+				System.out.println("B");
+			}else {
+				System.out.println("E");
+			}
+	}
+
+}
