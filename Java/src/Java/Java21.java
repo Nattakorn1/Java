919361560e858368b82d3c6730d99e6a0209c371
Nattakorn1 <natta.delltwin@gmail.com>
2021-03-10 13:32:56 +0700

    Java2.1

---
+++ Java/src/Java/Java21.java
@@ -0,0 +1,29 @@
+package Java;
+
+public class Java21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//2.1.1
+			//Hello Java
+			/* This is a double space bar
+			 	example 2.1.1
+			 */
+		
+		//2.1.2
+			int a = 1;
+			boolean b = true;
+			String c = "java";
+			float d = 1.1f;
+			double f = 0.0d;
+		//2.1.3
+			System.out.println("This is a String " + a);
+			System.out.println("This is a String " + b);
+			System.out.println("This is a String " + c);
+			System.out.println("This is a String " + d);
+			System.out.println("This is a String " + f);
+
+
+	}
+
+}
