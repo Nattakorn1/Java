11471e1109caf0abb1858074af1d31c418e1127e
Nattakorn1 <natta.delltwin@gmail.com>
2021-03-11 10:55:31 +0700

    Java5

---
+++ Java/src/Java/Java5.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class Java5 {
+	
+	public static void main(String[] args) {
+		//Lab5
+		int a = 1;
+		System.out.println(de(a));		
+	}
+	public static int de(int b) {
+		return b;
+	}
+}
